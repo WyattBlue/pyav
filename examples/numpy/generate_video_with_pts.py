c01335f3b5cad8794639906cc23919191c0c9d3a
WyattBlue <wyattblue@auto-editor.com>
2023-11-23 01:26:57 -0500

    Apply black and isort to examples, tests

--- examples/numpy/generate_video_with_pts.py
+++ examples/numpy/generate_video_with_pts.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-from fractions import Fraction
 import colorsys
+from fractions import Fraction
 
 import numpy as np
 
 import av
-
 
 (width, height) = (640, 360)
 total_frames = 20
@@ -43,7 +42,6 @@ block_w2 = int(0.5 * width / total_frames * 0.75)
 block_h2 = int(0.5 * height / 4)
 
 for frame_i in range(total_frames):
-
     # move around the color wheel (hue)
     nice_color = colorsys.hsv_to_rgb(frame_i / total_frames, 1.0, 1.0)
     nice_color = (np.array(nice_color) * 255).astype(np.uint8)
--- tests/test_encode.py
+++ tests/test_encode.py
@@ -1,14 +1,13 @@
+import math
 from fractions import Fraction
 from unittest import SkipTest
-import math
 
+import av
 from av import AudioFrame, VideoFrame
 from av.audio.stream import AudioStream
 from av.video.stream import VideoStream
-import av
 
 from .common import Image, TestCase, fate_suite
-
 
 WIDTH = 320
 HEIGHT = 240
