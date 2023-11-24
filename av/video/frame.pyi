2266f33a3512cd580c95610adf3e8486447dbf87
WyattBlue <wyattblue@auto-editor.com>
2023-11-24 01:38:51 -0500

    Add stubs for enum and video

---
+++ av/video/frame.pyi
@@ -0,0 +1,16 @@
+from typing import Any
+
+from .plane import VideoPlane
+
+class VideoFrame:
+    planes: tuple[VideoPlane]
+    width: int
+    height: int
+    key_frame: bool
+    interlaced_frame: bool
+    pict_type: Any
+
+    def from_image(img: Any) -> VideoFrame: ...
+    def __init__(
+        self, name: str, width: int = 0, height: int = 0, format: str = "yuv420p"
+    ): ...
