2bc7ffb1cbb1df26d7795ad831367e9fcfec588c
WyattBlue <wyattblue@auto-editor.com>
2023-11-29 00:22:48 -0500

    Add even more type stubs

--- av/__init__.pyi
+++ av/__init__.pyi
@@ -2,6 +2,7 @@ from av import error, logging
 
 from .audio.frame import *
 from .audio.layout import *
+from .audio.resampler import AudioResampler
 from .codec.codec import *
 from .container.core import *
 from .container.input import InputContainer
